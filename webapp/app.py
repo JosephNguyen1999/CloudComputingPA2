160a3a4d8bb299cdfbee290dafed1b84217eb8fd
JosephNguyen1999 <jhn21502@uga.edu>
2022-02-22 20:23:13 -0500

    test

--- webapp/app.py
+++ webapp/app.py
@@ -34,9 +34,31 @@ def upload_file():
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def index():
     return render_template("index.html")
+
+
+@app.route("/result")
+def submit():
+    if request.method == 'POST':
+        subprocess.call("rm -f ./a.out", shell=True)
+    retcode = subprocess.call("/usr/bin/g++ uploads/walk.cc", shell=True)
+    if retcode:
+        print("failed to compile walk.cc")
+    exit
+
+    subprocess.call("rm -f ./output", shell=True)
+    retcode = subprocess.call("./test.sh", shell=True)
+
+    print("Score: " + str(retcode) + " out of 2 correct.")
+
+    print("*************Original submission*************")
+    with open('uploads/walk.cc', 'r') as fs:
+        print(fs.read())
+
+    return render_template("result.html", fs)
 
 
 if __name__ == "__main__":

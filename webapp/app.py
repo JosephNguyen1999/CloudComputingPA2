e7cd3946ce588b94d39db63a716954a5447e7e6c
JosephNguyen1999 <jhn21502@uga.edu>
2022-02-23 00:10:00 -0500

    test

--- webapp/app.py
+++ webapp/app.py
@@ -56,17 +56,17 @@ def submit():
         subprocess.call("rm -f ./a.out", shell=True)
         retcode = subprocess.call("/usr/bin/g++ ./uploads/walk.cc", shell=True)
         if retcode:
-            print("failed to compile walk.cc")
+            result = "failed to compile walk.cc"
         exit    
 
         subprocess.call("rm -f ./output", shell=True)
         retcode = subprocess.call("./test.sh", shell=True)
 
-        print("Score: " + str(retcode) + " out of 2 correct.")
+        result = "Score: " + str(retcode) + " out of 2 correct."
 
-        print("*************Original submission*************")
+        result += "\n*************Original submission*************"
         with open('./uploads/walk.cc', 'r') as fs:
-            print(fs.read())
+            result += "\n(fs.read())"
     # if request.method == 'POST':
     #     subprocess.call("compile.sh", shell=True)
     # result = subprocess.check_output('python', 'compile.py')

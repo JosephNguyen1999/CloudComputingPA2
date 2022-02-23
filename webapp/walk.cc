6cc336f334f5ea6555c47f7c0c4d1870e5f999ce
JosephNguyen1999 <jhn21502@uga.edu>
2022-02-23 00:48:17 -0500

    wrong submission

--- webapp/walk.cc
+++ webapp/walk.cc
@@ -11,6 +11,5 @@ int main()
     getline(std::cin, name1);
     std::cout << "Enter a name:";
     getline(std::cin, name2);
-    std::cout << name1 + " and " + name2 + " went for a walk.\n";
-    getline
+    std::cout << " and went for a walk.\n";
 }
